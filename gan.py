6fb9bd702b4628eaaffa1a1a371699fdd891211c
Ted <ted.pinkerton@mail.utoronto.ca>
2021-01-18 01:47:52 -0500

    Fixed the crashing and now everything works
    Updating tensorflow, keras, CUDA, and CUDnn fixed the problems

--- gan.py
+++ gan.py
@@ -7,6 +7,8 @@ import glob
 from sklearn.utils import shuffle
 from PIL import Image
 import time
+
+import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
 from keras.layers import Dense, Reshape, Conv2D, Conv2DTranspose, Dropout, Input, merge
@@ -29,27 +31,27 @@ K.set_image_data_format("channels_last")
 
 # A function to normalize image pixels.
 def norm_img(img):
-    '''A function to Normalize Images.
+    """A function to Normalize Images.
     Input:
         img : Original image as numpy array.
     Output: Normailized Image as numpy array
-    '''
+    """
     img = (img / 127.5) - 1
     return img
 
 
 def denorm_img(img):
-    '''A function to Denormailze, i.e. recreate image from normalized image
+    """A function to Denormailze, i.e. recreate image from normalized image
     Input:
         img : Normalized image as numpy array.
     Output: Original Image as numpy array
-    '''
+    """
     img = (img + 1) * 127.5
     return img.astype(np.uint8) 
 
 
 def sample_from_dataset(batch_size, image_shape, data_dir=None):
-    '''Create a batch of image samples by sampling random images from a data directory.
+    """Create a batch of image samples by sampling random images from a data directory.
     Resizes the image using image_shape and normalize the images.
     Input:
         batch_size : Sample size required
@@ -58,7 +60,7 @@ def sample_from_dataset(batch_size, image_shape, data_dir=None):
 
     Output:
         sample : batch of processed images 
-    '''
+    """
     sample_dim = (batch_size, ) + image_shape
     sample = np.empty(sample_dim, dtype=np.float32)
     all_data_dirlist = list(glob.glob(data_dir))
@@ -66,11 +68,56 @@ def sample_from_dataset(batch_size, image_shape, data_dir=None):
     for index, img_filename in enumerate(sample_imgs_paths):
         image = Image.open(img_filename)
         image = image.resize(image_shape[:-1])
-        image = image.convert('RGB') 
+        image = image.convert("RGB") 
         image = np.asarray(image)
         image = norm_img(image)
         sample[index, ...] = image
     return sample
+
+
+def savegrid(ims, rows=None, cols=None, fill=True, showax=False):
+    if rows is None != cols is None:
+        raise ValueError("Set either both rows and cols or neither.")
+
+    if rows is None:
+        rows = len(ims)
+        cols = 1
+
+    gridspec_kw = {"wspace": 0, "hspace": 0} if fill else {}
+    fig,axarr = plt.subplots(rows, cols, gridspec_kw=gridspec_kw)
+
+    if fill:
+        bleed = 0
+        fig.subplots_adjust(left=bleed, bottom=bleed, right=(1 - bleed), top=(1 - bleed))
+
+    for ax,im in zip(axarr.ravel(), ims):
+        ax.imshow(im)
+        if not showax:
+            ax.set_axis_off()
+
+    kwargs = {"pad_inches": .01} if fill else {}
+    fig.savefig("faces.png", **kwargs)
+
+
+def save_img_batch(img_batch, img_save_dir):
+    """Takes as input a image batch and a img_save_dir and saves 16 images from the batch in a 4x4 grid in the img_save_dir
+    """
+    plt.figure(figsize=(16,16))
+    gs1 = gridspec.GridSpec(4, 4)
+    gs1.update(wspace=0, hspace=0)
+    rand_indices = np.random.choice(img_batch.shape[0],16,replace=False)
+    for i in range(16):
+        ax1 = plt.subplot(gs1[i])
+        ax1.set_aspect("equal")
+        rand_index = rand_indices[i]
+        image = img_batch[rand_index, :,:,:]
+        fig = plt.imshow(denorm_img(image))
+        plt.axis("off")
+        fig.axes.get_xaxis().set_visible(False)
+        fig.axes.get_yaxis().set_visible(False)
+    plt.tight_layout()
+    plt.savefig(img_save_dir,bbox_inches="tight",pad_inches=0)
+    # plt.show() 
 
 
 def gen_noise(batch_size, noise_shape):
@@ -163,12 +210,12 @@ def get_disc_normal(image_shape=(64, 64, 3)):
 
 
 class GAN:
-    def __init__(self, image_shape, noise_shape):
+    def __init__(self, image_shape, noise_shape, discriminator_save=None, generator_save=None):
         self.image_shape = image_shape
         self.noise_shape = noise_shape
 
-        self.discriminator = get_disc_normal(self.image_shape)
-        self.generator = get_gen_normal(self.noise_shape)
+        self.discriminator = get_disc_normal(self.image_shape) if discriminator_save == None else keras.models.load_model(discriminator_save)
+        self.generator = get_gen_normal(self.noise_shape) if generator_save == None else keras.models.load_model(generator_save)
 
         self.discriminator.trainable = False
 
@@ -186,14 +233,14 @@ class GAN:
 
 
 
-    def train(self, data_dir, num_steps=1000, batch_size=64, img_save_dir=None, log_dir=None):
+    def train(self, data_dir, num_steps=1000, batch_size=64, save_intervals=(10, 500), img_save_dir=None, log_dir=None):
         # Use a fixed noise vector to see how the GAN Images transition through time on a fixed noise. 
         fixed_noise = gen_noise(16, self.noise_shape)
 
         # To keep Track of losses
-        avg_disc_fake_loss = []
-        avg_disc_real_loss = []
-        avg_GAN_loss = []
+        self.avg_disc_fake_loss = []
+        self.avg_disc_real_loss = []
+        self.avg_GAN_loss = []
 
         # We will run for num_steps iterations
         for step in range(num_steps): 
@@ -208,15 +255,12 @@ class GAN:
             # Genearate noise to send as input to the generator
             noise = gen_noise(batch_size, self.noise_shape)
             
-            print("flag 1")
             # Use generator to create(predict) images
             fake_data_X = self.generator.predict(noise)
-            print("flag 2")
 
             # Save predicted images from the generator every 10th step
-            if img_save_dir != None and (tot_step % 100) == 0:
-                step_num = str(tot_step).zfill(4)
-                save_img_batch(fake_data_X, f"{img_save_dir}{step_num:05d}_image.png")
+            if img_save_dir != None and (tot_step % save_intervals[0]) == 0:
+                save_img_batch(fake_data_X, f"{img_save_dir}{tot_step:05d}_image.png")
 
             # Create the labels for real and fake data. We don"t give exact ones and zeros but add a small amount of noise. This is an important GAN training trick
             real_data_Y = np.ones(batch_size) - np.random.random_sample(batch_size)*0.2
@@ -227,15 +271,14 @@ class GAN:
             self.generator.trainable = False
 
             # Training Discriminator seperately on real data
-            dis_metrics_real = discriminator.train_on_batch(real_data_X, real_data_Y) 
+            dis_metrics_real = self.discriminator.train_on_batch(real_data_X, real_data_Y) 
             # training Discriminator seperately on fake data
-            dis_metrics_fake = discriminator.train_on_batch(fake_data_X, fake_data_Y) 
-            print("flag 1")
+            dis_metrics_fake = self.discriminator.train_on_batch(fake_data_X, fake_data_Y) 
             print("Disc: real loss: %f fake loss: %f" % (dis_metrics_real[0], dis_metrics_fake[0]))
             
             # Save the losses to plot later
-            avg_disc_fake_loss.append(dis_metrics_fake[0])
-            avg_disc_real_loss.append(dis_metrics_real[0])
+            self.avg_disc_fake_loss.append(dis_metrics_fake[0])
+            self.avg_disc_real_loss.append(dis_metrics_real[0])
             
             # Train the generator using a random vector of noise and its labels (1"s with noise)
             self.generator.trainable = True
@@ -254,18 +297,18 @@ class GAN:
             text_file.close()
 
             # save GAN loss to plot later
-            avg_GAN_loss.append(gan_metrics[0])
+            self.avg_GAN_loss.append(gan_metrics[0])
                     
             end_time = time.time()
             diff_time = int(end_time - step_begin_time)
             print("Step %d completed. Time took: %s secs." % (tot_step, diff_time))
             
             # save model at every 500 steps
-            if ((tot_step+1) % 500) == 0:
+            if ((tot_step+1) % save_intervals[1]) == 0:
                 print("-----------------------------------------------------------------")
-                print("Average Disc_fake loss: %f" % (np.mean(avg_disc_fake_loss))) 
-                print("Average Disc_real loss: %f" % (np.mean(avg_disc_real_loss))) 
-                print("Average GAN loss: %f" % (np.mean(avg_GAN_loss)))
+                print("Average Disc_fake loss: %f" % (np.mean(self.avg_disc_fake_loss))) 
+                print("Average Disc_real loss: %f" % (np.mean(self.avg_disc_real_loss))) 
+                print("Average GAN loss: %f" % (np.mean(self.avg_GAN_loss)))
                 print("-----------------------------------------------------------------")
                 self.discriminator.trainable = False
                 self.generator.trainable = False
@@ -304,7 +347,7 @@ if __name__ == "__main__":
     # Shape of noise vector to be input to the Generator
     noise_shape = (1,1,100)
     # Number of steps for training. num_epochs = num_steps*batch_size/data_size
-    num_steps = 200
+    num_steps = 2000
     # batch size for training.
     batch_size = 64
     # Location to save images and logs 
@@ -319,4 +362,4 @@ if __name__ == "__main__":
 
     gan = GAN(image_shape, noise_shape)
 
-    gan.train(data_dir, num_steps, batch_size, img_save_dir, log_dir)
+    gan.train(data_dir, num_steps, batch_size, (10, 10), img_save_dir, log_dir)
